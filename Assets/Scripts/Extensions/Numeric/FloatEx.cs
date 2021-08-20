6f0ae60d09db6ba519aa6c6472615429c74c8690
LyonSyonII <slimystudios@protonmail.com>
2021-08-20 20:47:38 +0200

    Float and Vector2 extensions

--- Assets/Scripts/Extensions/Numeric/FloatEx.cs
+++ Assets/Scripts/Extensions/Numeric/FloatEx.cs
@@ -1,40 +1,59 @@
 using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
 
 public static class FloatEx
 {
-        /// <summary>
-       /// Returns float sign.
-       /// </summary>
-        public static int Sign(this float number) 
-            => Math.Sign(number);
-        
-        /// <summary>
-        /// True if float is even.
-        /// </summary>
-        /// <returns></returns>
-        public static bool IsEven(this float number) 
-            => number % 2 == 0;
-        
-        /// <summary>
-        /// True if float is equal to other based on certain tolerance.
-        /// <para> Example: If tolerance is 0.1, 4.9f and 5f are equal.</para>
-        /// </summary>
-        public static bool IsEqual(this float number, float other, float tolerance)
-            => Math.Abs(number - other) < tolerance;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static float Round(this float f) 
+        => f >= 0 ? (int)(f+0.5f) : -(int)(0.5f-f);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int RoundToInt(this float f) 
+        => f >= 0 ? (int)(f+0.5f) : -(int)(0.5f-f);
+    
+    /// <summary>
+    /// Returns sign of float.
+    /// </summary>
+    /// <returns> 1 if value &#62; 0. -1 if value &#60; 0. 0 if value == 0. </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static float SignF(this float f) 
+        => Math.Sign(f);
+    
+    /// <summary>
+    /// Returns sign of float.
+    /// </summary>
+    /// <returns> 1 if value &#62; 0. -1 if value &#60; 0. 0 if value == 0. </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(this float f) 
+        => Math.Sign(f);
+    
+    /// <summary>
+    /// Returns sign of float.
+    /// </summary>
+    /// <returns> 1 if value &#62;= 0. -1 if value &#60; 0. </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static float MathfSign(this float f) 
+        => Mathf.Sign(f);
 
-        /// <summary>
-        /// True if float is equal to double based on certain tolerance.
-        /// <para> Example: If tolerance is 0.1, 4.9f and 5.0 are equal.</para>
-        /// </summary>
-        public static bool IsEqual(this float number, double other, float tolerance)
-            => Math.Abs(number - other) < tolerance;
+    /// <summary>
+    /// True if float is even.
+    /// </summary>
+    public static bool IsEven(this float number) 
+        => number % 2 == 0;
         
-        /// <summary>
-        /// True if float is equal to int based on certain tolerance.
-        /// <para> Example: If tolerance is 0.1, 4.9f and 5 are equal.</para>
-        /// </summary>
-        public static bool IsEqual(this float number, int other, float tolerance) 
-            => Math.Abs(number - other) < tolerance;
+    /// <summary>
+    /// True if float is equal to other based on certain tolerance.
+    /// <para> Example: If tolerance is 0.1, 4.9f and 5f are equal.</para>
+    /// </summary>
+    public static bool IsEqual(this float number, float other, float tolerance) 
+        => Math.Abs(number - other) < tolerance;
 
+    /// <summary>
+    /// True if float is equal to double based on certain tolerance.
+    /// <para> Example: If tolerance is 0.1, 4.9f and 5.0 are equal.</para>
+    /// </summary>
+    public static bool IsEqual(this float number, double other, float tolerance)
+        => Math.Abs(number - other) < tolerance;
         
+    /// <summary>
+    /// True if float is equal to int based on certain tolerance.
+    /// <para> Example: If tolerance is 0.1, 4.9f and 5 are equal.</para>
+    /// </summary>
+    public static bool IsEqual(this float number, int other, float tolerance) 
+        => Math.Abs(number - other) < tolerance;
 }
--- Assets/Scripts/Extensions/Vector/Vector2Ex.cs
+++ Assets/Scripts/Extensions/Vector/Vector2Ex.cs
@@ -1,41 +1,117 @@
+using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public static class Vector2Ex
 {
+    public static Vector3Int v3int;
+    public static Vector2Int v2int;
+    public static Vector3 v3;
+    public static Vector2 v2;
+    
+    #region Operators
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Vector2 FastMultiply(this Vector2 v, float f)
+    {
+        v.x *= f;
+        v.y *= f;
+        return v;
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Vector2 FastMultiply(this Vector2 v, Vector2 v2)
+    {
+        v.x *= v2.x;
+        v.y *= v2.y;
+        return v;
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Vector2 FastAdd(this Vector2 v, Vector2 v2)
+    {
+        v.x += v2.x;
+        v.y += v2.y;
+        return v;
+    }   
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Vector2 FastSubstract(this Vector2 v, Vector2 v2)
+    {
+        v.x -= v2.x;
+        v.y -= v2.y;
+        return v;
+    }
+    #endregion
+
+    #region Conversors
+
     /// <summary>
     /// Returns screen point as world coordinates.
     /// </summary>
     public static Vector3 ScreenToWorld(this Vector2 screenPoint) 
-    { 
-        return EasyExtensions.cam.ScreenToWorldPoint(screenPoint);
-    }
-    
+        => EasyExtensions.cam.ScreenToWorldPoint(screenPoint);
+
     /// <summary>
     /// Returns the vector as a Vector3Int. Uses basic float to int cast.
     /// </summary>
-    public static Vector3Int ToVector3Int(this Vector2 vector)
+    public static Vector3Int ToVector3Int(this Vector2 v)
     {
-        Vector3Int intExVec = new Vector3Int
-        {
-            x = (int) vector.x, 
-            y = (int) vector.y, 
-            z = 0
-        };
-
-        return intExVec;
+        v3int.x = (int)v.x;
+        v3int.y = (int)v.y;
+        v3int.z = 0;
+        return v3int;
     }
     
     /// <summary>
     /// Returns the vector as a Vector2Int. Uses basic float to int cast.
     /// </summary>
-    public static Vector2Int ToVector2Int(this Vector2 vector)
+    public static Vector2Int ToVector2Int(this Vector2 v)
     {
-        Vector2Int intVec = new Vector2Int
-        {
-            x = (int) vector.x, 
-            y = (int) vector.y,
-        };
-
-        return intVec;
+        v2int.x = (int)v.x;
+        v2int.y = (int)v.y;
+        return v2int;
     }
+
+    #endregion
+
+    #region Math
+
+    public static Vector2 Round(this Vector2 v) => v.SetXY(v.x.Round(), v.y.Round());
+    
+    public static Vector2 Raw(this Vector2 v) => v.SetXY(v.x.SignF(), v.y.SignF());
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Vector2 FastNormalized(this Vector2 v)
+    {
+        double m = Math.Sqrt(v.x * v.x + v.y * v.y);
+        if (m > 9.99999974737875E-06)
+        {
+            float fm = (float)m;
+            v.x /= fm;
+            v.y /= fm;
+            return v;
+        }
+            
+        return Vector2.zero;
+    }
+    
+    #endregion
+
+    #region Setters
+
+    public static Vector2 SetX(this Vector2 v, float x)
+    {
+        v.x = x;
+        return v;
+    }
+    
+    public static Vector2 SetY(this Vector2 v, float y)
+    {
+        v.y = y;
+        return v;
+    }
+    
+    public static Vector2 SetXY(this Vector2 v, float x, float y)
+    {
+        v.x = x;
+        v.y = y;
+        return v;
+    }
+
+    #endregion
 }
